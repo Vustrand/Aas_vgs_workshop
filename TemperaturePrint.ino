65e4792d8a95739333aaa7c60ce3d4bcd088ef24
Vustrand <121320428+Vustrand@users.noreply.github.com>
2023-03-26 12:54:42 +0200

    Add files via upload

---
+++ TemperaturePrint.ino
@@ -0,0 +1,23 @@
+#include <dht.h>
+
+#define dataPin 8 // Defines pin number to which the sensor is connected
+dht DHT; // Creats a DHT object
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int readData = DHT.read11(dataPin); // Reads the data from the sensor
+  float t = DHT.temperature; // Gets the values of the temperature
+  float h = DHT.humidity; // Gets the values of the humidity
+  
+  // Printing the results on the serial monitor
+  Serial.print("Temperature = ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print("    Humidity = ");
+  Serial.print(h);
+  Serial.println(" % ");
+  
+  delay(5000);
+}
